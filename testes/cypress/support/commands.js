af2c5eaa490e80fe4d231751c72628bff623a27d
Matheus Julidori <m.henrique@ges.inatel.br>
2022-10-31 18:04:11 -0300

    Relatório de testes

--- testes/cypress/support/commands.js
+++ testes/cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login',(username,password) => {
+    cy.visit("https://globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
+    cy.get('.btn-primary').click()
+})
